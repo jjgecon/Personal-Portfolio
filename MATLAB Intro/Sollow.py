12292caf3ee8a69bf3afc9138e170341b005e00c
jjgecon <javierj.g18@gmail.com>
2019-12-22 08:41:52 -0300

    Sollow Update

--- MATLAB Intro/Sollow.py
+++ MATLAB Intro/Sollow.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Set time period and grid space
-T,n = 10, 100
+T,n = 0.5, 100
 
 # Capital Grid
 k = np.linspace(0,T,n)
@@ -34,21 +34,33 @@ for i in k:
     depreciationlist.append(depreciation(i))
 
 # Last Checks
-tol = 0.009 # Tolerance level to 1 SS
+kss = []
+tol = 0.0009 # Tolerance level to 1 SS
 for i in k:
     if i == 0:
         a = 0
     if abs(savings(production(i)) - depreciation(i)) <= tol:
         print(f'Steady State at {i:.2f} capital')
-        print(f'Production Level at {production(i):.2f} capital')
+        print(f'Production Level at {production(i):.2f}')
         a += 1
+        kss.append(i) #used for graphing purposes
+
 
 if a == 0:
     print('No Steady State')
 
 # Graph
 fig, ax = plt.subplots(figsize=(10, 6))
+zoom = 0.5 #0.5 for a better zoom
+plt.axis([0, zoom, 0, zoom]) #Change this for a better zoom on the SS
 ax.plot(k,product)
 ax.plot(k,product_savings)
 ax.plot(k,depreciationlist)
+for i in kss:
+    line = []
+    b = np.linspace(0,8,n)
+    for e in k:
+        line.append(i)
+    ax.plot(line,b,'--r', alpha=0.2)
+
 plt.show()
